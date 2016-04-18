ce59c8f5b0acc1acafd2027443659176774b91c5
Akash Bharadwaj <akash.bharadwaj92@yahoo.com>
2016-04-18 01:52:12 -0400

    Implemented 3 view training accommodating all possible subsets of views except left-right

--- Model/bridge_corr_net.py
+++ Model/bridge_corr_net.py
@@ -576,7 +576,7 @@ def trainBridgeCorrNet(src_folder, tgt_folder, batch_size = 20, training_epochs=
             next = line.strip().split(",")
             if(next[0]=="xy"):
                 if(next[1]=="dense"):
-                    denseTheanoloader(next[2]+"_left",train_set_x_left,"float32")
+                    denseTheanoloader(next[2]+"_left",train_set_x_left, "float32")
                     denseTheanoloader(next[2]+"_right",train_set_x_right, "float32")
                     denseTheanoloader(next[2] + "_left", train_set_x_pivot, "float32")
                 else:
@@ -621,6 +621,207 @@ def trainBridgeCorrNet(src_folder, tgt_folder, batch_size = 20, training_epochs=
         detfile.close()
         # save the parameters for every 5 epochs
         if((epoch+1)%5==0):
+            model.save_matrices()
+
+    end_time = time.clock()
+    training_time = (end_time - start_time)
+    print ' code ran for %.2fm' % (training_time / 60.)
+    model.save_matrices()
+
+def trainBridgeCorrNet_with_mats(left_pivot_train=None, right_pivot_train=None, right_train=None, left_train=None, pivot_train=None, all_train=None, batch_size=20, training_epochs=40,
+                       l_rate=0.01, optimization="sgd", tied=False,  n_hidden=None, n_visible_left=None, n_visible_right=None,
+                       n_visible_pivot=None, lamda=5,
+                       W_left=None, W_right=None, W_pivot=None, b=None, W_left_prime=None, W_right_prime=None,
+                       W_pivot_prime=None, b_prime_left=None, b_prime_right=None, b_prime_pivot=None, hidden_activation="sigmoid",
+                       output_activation="sigmoid", loss_fn="squarrederror", tgt_folder="./"):
+    """
+    :param left_pivot_train: Dictionary {LEFT: left view train instances, PIVOT: pivot view train instances}
+    :param right_pivot_train: Dictionary {RIGHT: right view train instances, PIVOT: pivot view train instances}
+    :param right_train: Dictionary {RIGHT: right view train instances}
+    :param left_train: Dictionary {LEFT: left view train instances}
+    :param pivot_train: Dictionary {PIVOT: pivot view train instances}
+    :param all_train: Dictionary {LEFT: left view train instances, PIVOT: pivot view train instances, RIGHT: right view train instances}
+    """
+    index = T.lscalar()
+    x_left = T.matrix('x_left')
+    x_right = T.matrix('x_right')
+    x_pivot = T.matrix('x_pivot')
+    # The above three theano tensors indicate the input to the Bridge Corr Net
+
+    if left_pivot_train is not None or all_train is not None:
+        assert n_visible_left is not None, "n_visible_left not specified"
+        if left_pivot_train is not None:
+            assert n_visible_left == left_pivot_train[LEFT].shape[1], "n_visible_left and left_pivot mat dimension don't match"
+        if all_train is not None:
+            assert n_visible_left == all_train[LEFT].shape[1], "n_visible_left and all_train mat dimension don't match"
+        if left_train is not None:
+            assert n_visible_left == left_train.shape[1], "n_visible_left and left_train mat dimension don't match"
+
+    if right_pivot_train is not None or all_train is not None:
+        assert n_visible_right is not None, "n_visible_right not specified"
+        if right_pivot_train is not None:
+            assert n_visible_right == right_pivot_train[RIGHT].shape[1], "n_visible_right and right_pivot mat dimension don't match"
+        if all_train is not None:
+            assert n_visible_right == all_train[RIGHT].shape[1], "n_visible_right and all_train mat dimension don't match"
+        if right_train is not None:
+            assert n_visible_right == right_train.shape[1], "n_visible_right and right_train mat dimension don't match"
+
+    assert n_visible_pivot is not None, "n_visible_pivot MUST be specified"
+    if right_pivot_train is not None:
+        assert n_visible_pivot == right_pivot_train[PIVOT].shape[1], "n_visible_pivot and right_pivot mat dimension don't match"
+    if left_pivot_train is not None:
+        assert n_visible_pivot == left_pivot_train[PIVOT].shape[1], "n_visible_pivot and left_pivot don't match"
+    if all_train is not None:
+        assert n_visible_pivot == all_train[PIVOT].shape[1], "n_visible_pivot and all_train mat dimension don't match"
+    if pivot_train is not None:
+        assert n_visible_pivot == pivot_train.shape[1], "n_visible_pivot and pivot_train mat dimension don't match"
+
+
+    rng = numpy.random.RandomState(123)
+    theano_rng = RandomStreams(rng.randint(2 ** 30))
+
+    model = BridgeCorrNet()
+    model.init(numpy_rng=rng, theano_rng=theano_rng, l_rate=l_rate, optimization=optimization, tied=tied,
+               n_visible_left=n_visible_left, n_visible_right=n_visible_right, n_visible_pivot=n_visible_pivot,
+               n_hidden=n_hidden, lamda=lamda, W_left=W_left, W_right=W_right, W_pivot=W_pivot, b=b,
+               W_left_prime=W_left_prime, W_right_prime=W_right_prime, W_pivot_prime=W_pivot_prime,
+               b_prime_left=b_prime_left, b_prime_right=b_prime_right, b_prime_pivot=b_prime_pivot,
+               input_left=x_left, input_right=x_right, input_pivot=x_pivot, hidden_activation=hidden_activation,
+               output_activation=output_activation, loss_fn=loss_fn, op_folder=tgt_folder)
+    # model.load(tgt_folder,x_left,x_right)
+    start_time = time.clock()
+    train_set_x_left = theano.shared(numpy.asarray(numpy.zeros((1000, n_visible_left)), dtype=theano.config.floatX),
+                                     borrow=True)
+    train_set_x_right = theano.shared(numpy.asarray(numpy.zeros((1000, n_visible_right)), dtype=theano.config.floatX),
+                                      borrow=True)
+    train_set_x_pivot = theano.shared(numpy.asarray(numpy.zeros((1000, n_visible_pivot)), dtype=theano.config.floatX),
+                                      borrow=True)
+    # Dummy variables for input data. Borrow=true flag ensures the values can be updated later.
+
+    views = set()
+
+    # Note: The above theano shared variable assignments are just place holders and their actual values will be populated
+    # before actually calling the train method. This is also why borrow=True so that by changing the value of the nump mat
+    # used to initialize the shared variable, the shared variables value changes.
+
+    # common_cost, common_updates = model.train_common("1111")
+    common_cost, common_updates = model.train_common("1111")
+    mtrain_common = theano.function([index], common_cost, updates=common_updates,
+                                    givens=[(x_right, train_set_x_right[index * batch_size:(index + 1) * batch_size]),
+                                            (x_left, train_set_x_left[index * batch_size:(index + 1) * batch_size]),
+                                            (x_pivot, train_set_x_pivot[index * batch_size:(index + 1) * batch_size])])
+
+    right_pivot_cost, right_pivot_updates = model.train_right_pivot()
+    mtrain_right_pivot = theano.function([index], right_pivot_cost, updates=right_pivot_updates,
+                                   givens=[(x_right, train_set_x_right[index * batch_size:(index + 1) * batch_size]),
+                                           (x_pivot, train_set_x_pivot[index * batch_size:(index + 1) * batch_size])])
+
+    # left_pivot_cost, left_pivot_updates = model.train_right()
+    left_pivot_cost, left_pivot_updates = model.train_left_pivot()
+    mtrain_left_pivot = theano.function([index], left_pivot_cost, updates=left_pivot_updates,
+                                   givens=[(x_left, train_set_x_left[index * batch_size:(index + 1) * batch_size]),
+                                       (x_pivot, train_set_x_pivot[index * batch_size:(index + 1) * batch_size])])
+
+    left_cost, left_updates = model.train_left()
+    mtrain_left = theano.function([index], left_cost, updates=left_updates,
+                                  givens=[(x_left, train_set_x_left[index * batch_size:(index + 1) * batch_size])])
+
+    right_cost, right_updates = model.train_right()
+    mtrain_right = theano.function([index], right_cost, updates=right_updates,
+                                   givens=[(x_right, train_set_x_right[index * batch_size:(index + 1) * batch_size])])
+
+    pivot_cost, pivot_updates = model.train_pivot()
+    mtrain_pivot = theano.function([index], pivot_cost, updates=pivot_updates,
+                                   givens=[(x_pivot, train_set_x_pivot[index * batch_size:(index + 1) * batch_size])])
+
+    """
+        Note on what givens does:
+        Given usually takes a dict of { <tensor variable used in the model> : <value assigned to this tensor at run time/function call time
+                                                                               as a function of other variables/constants> }
+        Basically, it separates the actual tensor in the computation graph (part of the model definition) from a value to be
+        assigned to this variable (definition/assignment of the input variable).
+
+        Consequently, with the same model definition, using givens, at runtime we can substitute a node with a value that is
+        computed as a function of other (shared) variables, in this case: the input corresponding to that batch.
+
+        Why not just specify x_right/x_left/x_pivot as the input tensor to a theano compiled function and keep making calls to this
+        function by providing the appropriate batch training data matrix as the argument? Memory management can be optimized
+        by using a shared variable that will be loaded into the GPU initially, instead of passing a slice of data to the
+        theano function every time, which would require a transfer from CPU RAM to GPU each time. However, the model was defined
+        using x_right/x_left/x_pivot which are theano tensors. How do we make use of a shared variable to populate those tensors
+        without passing their value as input to the theano function? Using givens we can specify a slice of a theano shared variable (dataset)
+        to use for a specific function call, as a function of the batch index and the already loaded dataset living in a shared
+        variable on the GPU.
+    """
+
+    diff = 0
+    flag = 1
+    detfile = open(tgt_folder + "details.txt", "w")
+    detfile.close()
+    oldtc = float("inf")
+
+    for epoch in xrange(training_epochs):
+
+        print "in epoch ", epoch
+        c = []
+
+        if left_pivot_train is not None:
+            assert (left_pivot_train[LEFT].shape[0] == left_pivot_train[PIVOT].shape[0]), "No. of samples in left_pivot_train don't match!"
+            train_set_x_left.set_value(left_pivot_train[LEFT], borrow=True)
+            train_set_x_pivot.set_value(left_pivot_train[PIVOT], borrow=True)
+            data_size = left_pivot_train[LEFT].shape[0]
+            for batch_index in range(0, int(data_size / batch_size)):
+                c.append(mtrain_left_pivot(batch_index))
+        if all_train is not None:
+            assert (all_train[LEFT].shape[0] == all_train[RIGHT].shape[0] and all_train[RIGHT].shape[0] == all_train[PIVOT].shape[0]), "No. of samples in all_train don't match!"
+            train_set_x_left.set_value(all_train[LEFT], borrow=True)
+            train_set_x_right.set_value(all_train[RIGHT], borrow=True)
+            train_set_x_pivot.set_value(all_train[PIVOT], borrow=True)
+            data_size = all_train[LEFT].shape[0]
+            for batch_index in range(0, int(data_size / batch_size)):
+                c.append(mtrain_common(batch_index))
+        if right_pivot_train is not None:
+            assert (right_pivot_train[RIGHT].shape[0] == right_pivot_train[PIVOT].shape[
+                0]), "No. of samples in right_pivot_train don't match!"
+            train_set_x_right.set_value(right_pivot_train[RIGHT], borrow=True)
+            train_set_x_pivot.set_value(right_pivot_train[PIVOT], borrow=True)
+            data_size = right_pivot_train[RIGHT].shape[0]
+            for batch_index in range(0, int(data_size / batch_size)):
+                c.append(mtrain_right_pivot(batch_index))
+        if left_train is not None:
+            train_set_x_left.set_value(left_train, borrow=True)
+            data_size = left_train.shape[0]
+            for batch_index in range(0, int(data_size / batch_size)):
+                c.append(mtrain_left(batch_index))
+        if right_train is not None:
+            train_set_x_right.set_value(right_train, borrow=True)
+            data_size = right_train.shape[0]
+            for batch_index in range(0, int(data_size / batch_size)):
+                c.append(mtrain_right(batch_index))
+        if pivot_train is not None:
+            train_set_x_pivot.set_value(pivot_train, borrow=True)
+            data_size = pivot_train.shape[0]
+            for batch_index in range(0, int(data_size / batch_size)):
+                c.append(mtrain_pivot(batch_index))
+
+
+
+        if (flag == 1):
+            flag = 0
+            diff = numpy.mean(c)
+            di = diff
+        else:
+            di = numpy.mean(c) - diff
+            diff = numpy.mean(c)
+
+        print 'Difference between 2 epochs is ', di
+        print 'Training epoch %d, cost ' % epoch, diff
+
+        detfile = open(tgt_folder + "details.txt", "a")
+        detfile.write("train\t" + str(diff) + "\n")
+        detfile.close()
+        # save the parameters for every 5 epochs
+        if ((epoch + 1) % 5 == 0):
             model.save_matrices()
 
     end_time = time.clock()
--- mnistExample/train_bridge_corrnet.py
+++ mnistExample/train_bridge_corrnet.py
@@ -1,6 +1,6 @@
 __author__ = 'Sarath'
-
 import sys
+import numpy as np
 
 sys.path.append("../Model/")
 # from Model.bridge_corr_net import *
@@ -9,8 +9,8 @@ from mnist import *
 
 MNIST_DATA_PATH = "../mnist_images/"
 
-src_folder = sys.argv[1]+"matpic1/"
-tgt_folder = sys.argv[2]
+# src_folder = sys.argv[1]+"matpic1/"
+# tgt_folder = sys.argv[2]
 
 batch_size = 100
 training_epochs = 50
@@ -25,12 +25,82 @@ hidden_activation = "sigmoid"
 output_activation = "sigmoid"
 loss_fn = "squarrederror"
 
-# train_mnist_images, train_mnist_labels = load_mnist(dataset="training", digits=None, path=MNIST_DATA_PATH, asbytes=False, selection=None, return_labels=True, return_indices=False)
+def get_left_view(image):
+    left_view = []
+    for row in image:
+        left_view.append(row[:len(row)/2])
+    left_view = np.array(np.array(left_view).flatten())
+    return(np.array(left_view))
 
+def get_right_view(image):
+    right_view = []
+    for row in image:
+        right_view.append(row[len(row)/2:])
+    right_view = np.array(np.array(right_view).flatten())
+    return(right_view)
 
-trainBridgeCorrNet(src_folder=src_folder, tgt_folder=tgt_folder, batch_size=batch_size,
-             training_epochs=training_epochs, l_rate=l_rate, optimization=optimization,
-             tied=tied, n_visible_left=n_visible_left, n_visible_right=n_visible_right, n_visible_pivot=n_visible_right,
-             n_hidden=n_hidden, lamda=lamda, hidden_activation=hidden_activation,
-             output_activation=output_activation, loss_fn=loss_fn)
+def get_noise_view(image):
+    rand_noise = np.random.uniform(high=0.01, low=-0.01, size=(image.shape[0], image.shape[1]))
+    noise_view = np.array(np.array(image + rand_noise).flatten())
+    return(noise_view)
 
+def get_views(mnist_images, views=[LEFT, RIGHT, PIVOT]):
+    left_views = []
+    right_views = []
+    noise_views = []
+    for image in mnist_images:
+        if LEFT in views:
+            left_views.append(get_left_view(image))
+        if RIGHT in views:
+            right_views.append(get_right_view(image))
+        if PIVOT in views:
+            noise_views.append(get_noise_view(image))
+    ret_list = []
+    for view in views:
+        if view == LEFT:
+            ret_list.append(np.array(left_views))
+        if view == RIGHT:
+            ret_list.append(np.array(right_views))
+        if view == PIVOT:
+            ret_list.append(np.array(noise_views))
+    return(ret_list)
+
+train_mnist_images, train_mnist_labels = load_mnist(dataset="training", digits=None, path=MNIST_DATA_PATH, asbytes=False, selection=None, return_labels=True, return_indices=False)
+
+tot_train_len = len(train_mnist_images)
+
+[all_train_left, all_train_right, all_train_pivot] = get_views(train_mnist_images[:tot_train_len/6], views=[LEFT, RIGHT, PIVOT])
+print(all_train_left.shape, all_train_right.shape, all_train_pivot.shape)
+all_train = {LEFT: all_train_left, RIGHT: all_train_right, PIVOT: all_train_pivot}
+n_visible_left = len(all_train_left[0])
+n_visible_right = len(all_train_right[0])
+n_visible_pivot = len(all_train_pivot[0])
+
+[left_pivot_left, left_pivot_pivot] = get_views(train_mnist_images[tot_train_len/6:tot_train_len * 2/6], views=[LEFT, PIVOT])
+print(left_pivot_left.shape, left_pivot_pivot.shape)
+left_pivot = {LEFT: left_pivot_left, PIVOT: left_pivot_pivot}
+
+[right_pivot_right, right_pivot_pivot] = get_views(train_mnist_images[tot_train_len*2/6 : tot_train_len * 3/6], views=[RIGHT, PIVOT])
+print(right_pivot_right.shape, right_pivot_pivot.shape)
+right_pivot = {RIGHT: right_pivot_right, PIVOT: right_pivot_pivot}
+
+[right_only] = get_views(train_mnist_images[tot_train_len*3/6 : tot_train_len * 4/6], views=[RIGHT])
+print(right_only.shape)
+[left_only] = get_views(train_mnist_images[tot_train_len*4/6 : tot_train_len * 5/6], views=[LEFT])
+print(left_only.shape)
+[pivot_only] = get_views(train_mnist_images[tot_train_len*5/6 : ], views=[PIVOT])
+print(pivot_only.shape)
+
+# trainBridgeCorrNet(src_folder=src_folder, tgt_folder=tgt_folder, batch_size=batch_size,
+#              training_epochs=training_epochs, l_rate=l_rate, optimization=optimization,
+#              tied=tied, n_visible_left=n_visible_left, n_visible_right=n_visible_right, n_visible_pivot=n_visible_right,
+#              n_hidden=n_hidden, lamda=lamda, hidden_activation=hidden_activation,
+#              output_activation=output_activation, loss_fn=loss_fn)
+
+trainBridgeCorrNet_with_mats(left_pivot_train=left_pivot, right_pivot_train=right_pivot, right_train=right_only, left_train=left_only,
+                                 pivot_train=pivot_only, all_train=all_train, batch_size=batch_size, training_epochs=training_epochs,
+                                 l_rate=l_rate, optimization=optimization, tied=tied, n_hidden=n_hidden, n_visible_left=n_visible_left,
+                                 n_visible_right=n_visible_right,
+                                 n_visible_pivot=n_visible_pivot, lamda=lamda,
+                                 hidden_activation=hidden_activation,
+                                 output_activation=output_activation, loss_fn=loss_fn, tgt_folder="./")
